9f82a6842d14cc422b94fc5be9553403b6ed1aaf
MeetShoaibKhan <meetshoaibkhan1@gmail.com>
2022-08-17 23:04:44 +0500

    second

--- assets/js/education.js
+++ assets/js/education.js
@@ -112,21 +112,21 @@ const showCards = () => {
   let output = "";
   moocscards.forEach(
     ({ title, cardImage, moocLink }) =>
-      (output += `        
-        <div class="col-6 col-md-3 col-sm-4 column" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="600" >  
+      (output += `
+        <div class="col-6 col-md-3 col-sm-4 column" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="600" >
             <div class="card mb-3 mx-auto">
                <div class="content">
                   <div class="content-overlay"></div>
-                    <img src=${cardImage} class="card-img-top content-image">     
+                    <img src=${cardImage} class="card-img-top content-image">
                   <div class="content-details fadeIn-bottom">
-                    <a href="${moocLink}" target="_blank"><i class="fa fa-info-circle fa-2x" aria-hidden="true" style="color: white;"></i></a>                                   
+                    <a href="${moocLink}" target="_blank"><i class="fa fa-info-circle fa-2x" aria-hidden="true" style="color: white;"></i></a>
                   </div>
                 </div>
                 <div class="card-body">
                     <h6 class="mt-0 py-2 text-center font-weight-bold mooc-title" style="font-size:12px;">${title}</h6>
                 </div>
             </div>
-        </div>        
+        </div>
       `)
   );
   moocs.innerHTML = output;
@@ -158,8 +158,8 @@ const showCards1 = () => {
   let output = "";
   badgesection.forEach(
     ({ title, image, description }) =>
-      (output += `       
-      <div class="col-lg-4 col-md-6 p-2" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="600"> 
+      (output += `
+      <div class="col-lg-4 col-md-6 p-2" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="600">
         <img class="img-fluid d-block mb-3 mx-auto hvr-grow" src="${image}" alt="Card image cap" width="200">
           <div class="text-center font-weight-bolder" style="font-size: 1.3em;">${title}</div>
           <div class="text-center text-muted font-weight-bolder p-2">${description}</div>
--- assets/js/references.js
+++ assets/js/references.js
@@ -7,35 +7,51 @@ const referenceTable = document.querySelector(".pain");
 const references = [
   {
     sl1: 1,
-    name1 : "Smaranjit Ghose",
-        designation1 : "Product Manager, Microsoft",
+    name1 : "Umair Jibran",
+        designation1 : "Associate Full Stack Engineer, ProductBox",
         image1 : "https://avatars.githubusercontent.com/u/46641503?v=4",
-        message1 : "John is a conscientious individual whom I mentored during JWOC'2021. His problem-solving skills and a keen interest in learning new skillsets are remarkable. His hardworking nature and team-building abilities make him different from others.",
-        
+        message1 : "Shoaib is an experienced backend developer. He has a good grasp on all the basics of programming and can usually get to the root of any problem. He's able to work well with clients and other teams, which often makes him a go-to person when we need technical help.",
+
 
         sl2:2,
-        name2 : "Anush Bhatia",
-        designation2 : "Senior Analyst, Capgemini", 
+        name2 : "M. Yaseen",
+        designation2 : "ML Engineer, Freelancer",
         image2: "https://avatars.githubusercontent.com/u/40017559?v=4",
-        message2 : "John has the best grasping skills I have seen in a new developer. I got to know him through GSSOC'2021 when I mentored him, and he made an exceptional growth during the competition. He made some incredible contributions and has good experience with Open Source.",
-        
-        
+        message2 : "Shoaib has the best grasping skills I have seen in a new developer. I got to know him through GSSOC'2021 when I mentored him, and he made an exceptional growth during the competition. He made some incredible contributions and has good experience with Open Source.",
+
+
     },
-
-    {
-        sl1:3,
-        name1 : "Aditya Jyoti Paul",
-        designation1 : "Technical Analyst, Amazon",
-        image1 : "https://media-exp1.licdn.com/dms/image/C5603AQGEV-kzawPxjw/profile-displayphoto-shrink_800_800/0/1588602102622?e=1651708800&v=beta&t=8nxhFoR-WB4OKVltyYaVjmRpig_kQcvFsoa-eLjJPBQ",
-        message1 : "John and I have worked on several projects. Apart from technical skills, he also has good soft skills. He has good experience with debugging and has excellent leadership qualities.",
-        
-
-        sl2:4,
-        name2 : "Arnab Dutta Purkayastha",
-        designation2 : "Data Scientist, Flipkart",
-        image2 : "https://avatars.githubusercontent.com/u/59832779?v=4",
-        message2 : "John and I worked together during a hackathon and the thing that surprised me the most about him was his knowledge of fundamentals. He offered sincere help to the team throughout the competition. He is an impressive problem solver who is able to address complex issues strategically and confidently.",
-  },
+  // {
+  //   sl1: 1,
+  //   name1 : "Smaranjit Ghose",
+  //       designation1 : "Product Manager, Microsoft",
+  //       image1 : "https://avatars.githubusercontent.com/u/46641503?v=4",
+  //       message1 : "John is a conscientious individual whom I mentored during JWOC'2021. His problem-solving skills and a keen interest in learning new skillsets are remarkable. His hardworking nature and team-building abilities make him different from others.",
+  //
+  //
+  //       sl2:2,
+  //       name2 : "Anush Bhatia",
+  //       designation2 : "Senior Analyst, Capgemini",
+  //       image2: "https://avatars.githubusercontent.com/u/40017559?v=4",
+  //       message2 : "John has the best grasping skills I have seen in a new developer. I got to know him through GSSOC'2021 when I mentored him, and he made an exceptional growth during the competition. He made some incredible contributions and has good experience with Open Source.",
+  //
+  //
+  //   },
+  //
+  //   {
+  //       sl1:3,
+  //       name1 : "Aditya Jyoti Paul",
+  //       designation1 : "Technical Analyst, Amazon",
+  //       image1 : "https://media-exp1.licdn.com/dms/image/C5603AQGEV-kzawPxjw/profile-displayphoto-shrink_800_800/0/1588602102622?e=1651708800&v=beta&t=8nxhFoR-WB4OKVltyYaVjmRpig_kQcvFsoa-eLjJPBQ",
+  //       message1 : "John and I have worked on several projects. Apart from technical skills, he also has good soft skills. He has good experience with debugging and has excellent leadership qualities.",
+  //
+  //
+  //       sl2:4,
+  //       name2 : "Arnab Dutta Purkayastha",
+  //       designation2 : "Data Scientist, Flipkart",
+  //       image2 : "https://avatars.githubusercontent.com/u/59832779?v=4",
+  //       message2 : "John and I worked together during a hackathon and the thing that surprised me the most about him was his knowledge of fundamentals. He offered sincere help to the team throughout the competition. He is an impressive problem solver who is able to address complex issues strategically and confidently.",
+  // },
 ];
 
 AOS.init();
@@ -57,7 +73,7 @@ const fillData = () => {
       message2,
       absbox_for_linkedin2,
     }) =>
-      (output += `<tr data-aos="zoom-in-left"> 
+      (output += `<tr data-aos="zoom-in-left">
                 <td class="imgCol"><img src="${image1}" class="rImg"></td>
                 <td class = "referenceTitleName">
                     <div>
@@ -66,15 +82,15 @@ const fillData = () => {
                         </span>
                     </div>
                     <img src="https://img.icons8.com/fluency/48/000000/quote-left.png"/>
-                    <a href="#0" class="paperTitle"> ${name1} </a> 
-                    <div> ${designation1} </div> <div class="rConferences">  
+                    <a href="#0" class="paperTitle"> ${name1} </a>
+                    <div> ${designation1} </div> <div class="rConferences">
                         <div class="referenceY">${message1}</div>
-                    </div>  
+                    </div>
                 </td>
-            </tr> 
-            
-            <tr data-aos="zoom-in-left"> 
-                       
+            </tr>
+
+            <tr data-aos="zoom-in-left">
+
                         <td class = "referenceTitleName">
                             <div>
                                 <span class="imgResponsive">
@@ -82,14 +98,14 @@ const fillData = () => {
                                 </span>
                             </div>
                             <img src="https://img.icons8.com/fluency/48/000000/quote-left.png"/>
-                            <a href="#0" class="paperTitle">${name2} </a> 
+                            <a href="#0" class="paperTitle">${name2} </a>
                             <div> ${designation2} </div> <div class="rConferences">
                                 <div class="referenceY">${message2}</div>
                             </div>
-                
-                            
-                            
-                           
+
+
+
+
                         </td>
                         <td class="imgCol"><img src="${image2}" class="rImg1"></td>
                     </tr>`)
